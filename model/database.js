647a9d18e6849dcfe96a2eaad3067bbd714b24a5
Steph-Aniee <119601242+Steph-Aniee@users.noreply.github.com>
2023-08-01 23:52:04 +0200

    debugging database connection

--- model/database.js
+++ model/database.js
@@ -8,10 +8,10 @@ const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "127.0.0.1",
+  host: DB_HOST || "containers-us-west-47.railway.app",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "nutri_profiles",
+  database: DB_NAME || "railway",
   multipleStatements: true,
 });
 
