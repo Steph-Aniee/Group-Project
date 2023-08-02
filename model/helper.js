567b9215ae1eb2fcc8e357c6ea29838f51e2c485
Steph-Aniee <119601242+Steph-Aniee@users.noreply.github.com>
2023-08-02 11:42:34 +0200

    prt added to database and helper files

--- model/helper.js
+++ model/helper.js
@@ -11,12 +11,14 @@ module.exports = async function db(query) {
     const DB_USER = process.env.DB_USER;
     const DB_PASS = process.env.DB_PASS;
     const DB_NAME = process.env.DB_NAME;
+    const DB_PORT = process.env.DB_PORT;
 
     const con = mysql.createConnection({
       host: DB_HOST,
       user: DB_USER,
       password: DB_PASS,
       database: DB_NAME,
+      port: DB_PORT,
       multipleStatements: true,
     });
 
